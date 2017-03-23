c9136496797a6b471144cc84b696144df93d96b8
Hyunseung Bang <bang@Hyunseungs-MacBook-Pro.local>
2017-03-22 23:57:25 -0400

    Implements getOrbitList and getInstrumentList functions that connect to VASSAR java server to get information.

---
+++ VASSAR_API/PythonClient.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements. See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership. The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+import sys
+import glob
+sys.path.append("/Users/bang/workspace/daphne/daphne-brain/VASSAR_API/gen-py")
+
+from pyInterface import VASSARInterface
+
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+class pyClient():
+    
+    def __init__(self):
+        # Make socket
+        self.transport = TSocket.TSocket('localhost', 9090)
+    
+        # Buffering is critical. Raw sockets are very slow
+        self.transport = TTransport.TBufferedTransport(self.transport)
+    
+        # Wrap in a protocol
+        self.protocol = TBinaryProtocol.TBinaryProtocol(self.transport)
+        
+        # Create a client to use the protocol encoder
+        self.client = VASSARInterface.Client(self.protocol)
+        
+    
+    def startConnection(self):
+        # Connect
+        self.transport.open()
+
+    
+    def endConnection(self):
+        # Close
+        self.transport.close()
+        
+    def initializeJess(self):
+        message = self.client.initJess()
+        print(message)
+        return message
+    
+    def evaluateArchitecture(self):
+        arch = self.client.eval('')
+        print('Test arch evaluated. Science: {0}, Cost: {1}'.format(arch.science, arch.cost))
+        return arch
+        
+    def ping(self):
+        self.client.ping()
+        print('ping()')
+        
+    def getOrbitList(self):
+        return self.client.getOrbitList()
+    
+    def getInstrumentList(self):
+        return self.client.getInstrumentList()
+
+    
+
+if __name__ == '__main__':
+    try:
+        #main()
+        pass
+    except Thrift.TException as tx:
+        print('%s' % tx.message)
--- iFEED_API/views.py
+++ iFEED_API/views.py
@@ -9,23 +9,32 @@ import sys,os
 import json
 import csv
 
+root = "/Users/bang/workspace/daphne/daphne-brain"
+root_ifeed = "/Users/bang/workspace/daphne/iFEED"
+sys.path.append(root+'/VASSAR_API')
+
+from PythonClient import pyClient
+
 # Create your views here.
 class IFEEDServer(APIView):
     """
-    IFEEDServer - testing
+    IFEEDServer
     """
     def __init__(self):
-        self.root = "/Users/bang/workspace/daphne/iFEED"
+        self.VASSARClient = pyClient()
         #self.http_method_names = ['get','post'];
         pass
     
     def post(self, request, format=None):
         
+        # Start connection with VASSAR
+        self.VASSARClient.startConnection()
         requestID = request.POST['ID']
+        output = ''
         
         if(requestID=='import_new_data'):
             # Set the path of the file containing data
-            file_path = self.root + request.POST['path']
+            file_path = root_ifeed + request.POST['path']
             # Open the file
             with open(file_path) as csvfile:
                 # Read the file as a csv file
@@ -33,14 +42,46 @@ class IFEEDServer(APIView):
                 architectures = []
                 # For each row, store the information
                 for ind, row in enumerate(read):
+                    # Change boolean string to boolean array
                     bitString = booleanString2booleanArray(row[0])
                     science = float(row[1])
                     cost = float(row[2])
-                    architectures = np.append(architectures,{'id':ind,'bitString':bitString,'science':science,'cost':cost})
-            return Response(architectures.tolist())
-        elif(requestID==''):
-            pass
+                    architectures.append({'id':ind,'bitString':bitString,'science':science,'cost':cost})
+            output = architectures
+
         
+        elif(requestID=='get_orbit_list'):
+            list = self.VASSARClient.getOrbitList()
+            output = list
+        
+        elif(requestID=='get_instrument_list'):
+            list = self.VASSARClient.getInstrumentList()
+            output = list
+
+        elif(requestID=='extract_info_from_bitString'):
+            bitString = request.POST['bitString']
+            orbitList = self.VASSARClient.getOrbitList()
+            instrList = self.VASSARClient.getInstrumentList()
+            # Compute the number of orbits and instruments
+            norb = len(orbitList)
+            ninstr = len(instrList)
+            
+            architecture = []
+            for i in range(norb):
+                orbit = orbitList[i]
+                assigned = []
+                for j in range(ninstr):
+                    if bitString[i*ninstr+j]=='1':
+                        instrument = instrList[j]
+                        # Store the instrument names assigned to jth orbit
+                        assigned.append(instrument)
+                # Store the name of the orbit and the assigned instruments
+                architecture.append({'orbit':orbit,'children':assigned})
+            output = architecture
+        
+        # End the connection before return statement
+        self.VASSARClient.endConnection()
+        return Response(output)
         
     def get(self, request, format=None):
         return Response({'test':'ifeed_get'})
@@ -51,26 +92,10 @@ def booleanString2booleanArray(booleanString):
     leng = len(booleanString)
     boolArray = []
     for i in range(leng):
-        if booleanString[i]==0:
+        if booleanString[i]=='0':
             boolArray.append(False)
         else:
             boolArray.append(True)
     return boolArray
 
 
-
-
-
-
-class InstrumentList(APIView):
-    """
-    List all instruments.
-    """
-    def get(self, request, format=None):
-        # Call Java library to get the list of instruments
-        # ArrayList<String> instrumentList = new ArrayList<>();
-        # String[] instruments = Params.instrument_list;
-        # for (String inst:instruments){
-        #     instrumentList.add(inst);
-        # }
-        return Response({'test2':'test2'})
