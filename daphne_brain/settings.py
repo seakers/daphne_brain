5ac72474fa1bf9de145adcf926091621073a06fb
Hyunseung Bang <bang@dhcp-rhodes-2458.redrover.cornell.edu>
2017-03-28 10:50:48 -0400

    Modifies nginx config to not use uwsgi and just set a proxy to URL with asgi port

--- daphne_brain/settings.py
+++ daphne_brain/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'zy&+i575bdsi3ytivu*iv7c-3)r88_g6oykwnb*yt34imq&)81'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '10.148.11.80',
     '10.0.2.2',
+    '127.0.0.1',
     'localhost'
 ]
 
--- iFEED_API/consumers.py
+++ iFEED_API/consumers.py
@@ -1,6 +1,32 @@
+from channels import Group
+
 def ws_message(message):
     # ASGI WebSocket packet-received and send-packet message types
     # both have a "text" key for their textual data.
     message.reply_channel.send({
         "text": message.content['text'],
     })
+    
+    
+# @receiver(post_save, sender=BlogUpdate)
+# def send_update(sender, instance, **kwargs):
+#     Group("liveblog").send({
+#         "text": json.dumps({
+#             "id": instance.id,
+#             "content": instance.content
+#         })
+#     })
+
+
+
+# Connected to websocket.connect
+def ws_connect(message):
+    # Add to the group
+    Group("ifeed").add(message.reply_channel)
+    # Accept the connection request
+    message.reply_channel.send({"accept": True})
+
+# Connected to websocket.disconnect
+def ws_disconnect(message):
+    # Remove from the group on clean disconnect
+    Group("ifeed").discard(message.reply_channel)
