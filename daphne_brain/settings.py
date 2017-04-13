3a9463a10462753a42da753d437958c7fe9633d2
Antoni Virós Martin <ani300@gmail.com>
2017-04-13 00:39:26 -0400

    Added allowed_hosts

--- daphne_brain/settings.py
+++ daphne_brain/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
