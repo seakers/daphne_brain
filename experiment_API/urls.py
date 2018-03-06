793e45e18b6a0997c933f2018f9cd7cf435ebc07
Antoni Virós Martin <ani300@gmail.com>
2018-03-06 18:12:51 -0500

    Maturing experiment API

--- experiment_API/urls.py
+++ experiment_API/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('start-experiment', views.StartExperiment.as_view()),
-    path('start-stage', views.StartStage.as_view()),
-    path('finish-stage', views.FinishStage.as_view()),
-    path('add-action', views.AddAction.as_view()),
+    path('start-stage/<int:stage>', views.StartStage.as_view()),
+    path('finish-stage/<int:stage>', views.FinishStage.as_view()),
+    path('add-action/<int:stage>', views.AddAction.as_view()),
     path('update-state', views.UpdateState.as_view()),
     path('reload-experiment', views.ReloadExperiment.as_view()),
-    path('finish-experiment', views.EndExperiment.as_view())
+    path('finish-experiment', views.FinishExperiment.as_view())
 ]
