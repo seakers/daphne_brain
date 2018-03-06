b4abf0098dd44691f0aa15fa082f7e8699cb72f5
Antoni Virós Martin <ani300@gmail.com>
2018-03-06 14:39:35 -0500

    Finished basic porting and reworked experiment APIs

--- data_mining_API/urls.py
+++ data_mining_API/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'get-driving-features/$', views.GetDrivingFeatures.as_view()),
-    url(r'get-marginal-driving-features-conjunctive/$', views.GetMarginalDrivingFeaturesConjunctive.as_view()),
-    url(r'get-marginal-driving-features/$', views.GetMarginalDrivingFeatures.as_view()),
-    
+    path('get-driving-features', views.GetDrivingFeatures.as_view()),
+    path('get-marginal-driving-features-conjunctive', views.GetMarginalDrivingFeaturesConjunctive.as_view()),
+    path('get-marginal-driving-features', views.GetMarginalDrivingFeatures.as_view())
 ]
--- experiment_API/urls.py
+++ experiment_API/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'start-experiment$', views.StartExperiment.as_view()),
-    url(r'start-stage$', views.StartStage.as_view()),
-    url(r'finish-stage$', views.FinishStage.as_view()),
-    url(r'add-action$', views.AddAction.as_view()),
-    url(r'update-state$', views.UpdateState.as_view()),
-    url(r'reload-experiment$', views.ReloadExperiment.as_view()),
-    url(r'finish-experiment$', views.EndExperiment.as_view())
+    path('start-experiment', views.StartExperiment.as_view()),
+    path('start-stage', views.StartStage.as_view()),
+    path('finish-stage', views.FinishStage.as_view()),
+    path('add-action', views.AddAction.as_view()),
+    path('update-state', views.UpdateState.as_view()),
+    path('reload-experiment', views.ReloadExperiment.as_view()),
+    path('finish-experiment', views.EndExperiment.as_view())
 ]
