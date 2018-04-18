a4c51924997867e3a30c605c20216b6b33173305
Antoni Virós Martin <ani300@gmail.com>
2018-04-18 15:00:06 -0400

    refactored command answering

--- daphne_API/qa_pipeline.py
+++ daphne_API/qa_pipeline.py
@@ -63,14 +63,15 @@ def classify(question, module_name):
 def load_type_info(question_type, module_name):
     with open('./daphne_API/command_types/' + module_name + '/' + str(question_type) + '.json', 'r') as file:
         type_info = json.load(file)
-    information = []
-    information.append(type_info["params"])
+    information = {}
+    information["type"] = type_info["type"]
+    information["params"] = type_info["params"]
     if type_info["type"] == "db_query":
-        information.append(type_info["query"])
+        information["query"] = type_info["query"]
     elif type_info["type"] == "run_function":
-        information.append(type_info["function"])
-    information.append(type_info["voice_response"])
-    information.append(type_info["visual_response"])
+        information["function"] = type_info["function"]
+    information["voice_response"] = type_info["voice_response"]
+    information["visual_response"] = type_info["visual_response"]
     return information
 
 extract_function = {}
