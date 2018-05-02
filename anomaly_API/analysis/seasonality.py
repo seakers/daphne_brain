f4b5c064386cffae0c7525a300218745cec7e3ad
CarlosLing <37625533+CarlosLing@users.noreply.github.com>
2018-05-02 10:19:40 -0400

    Added Seasonality analysis API

---
+++ anomaly_API/analysis/seasonality.py
@@ -0,0 +1,90 @@
+import logging
+
+from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+import numpy as np
+import pandas as pd
+import json
+from statsmodels.tsa.stattools import acf
+import matplotlib.pyplot as plt
+
+
+class CheckSeasonality(APIView):
+
+    def post(self, request, format=None):
+
+        data = pd.read_json(request.data['data'], orient='records').set_index('timestamp')
+        #
+        # data = pd.read_csv('anomaly_API/Data/sample.csv', parse_dates=True, index_col='timestamp')
+
+        if 'variable' in request.data:
+            variable = request.data['variable']
+        else:
+            variable = data.columns[0]
+
+        # input: maximum lag allowed
+        if 'n' in request.data:
+            n = request.data['n']
+        else:
+            n = 100
+
+        m = int(n/2)  # margin
+
+        probability_threshold = 0.5
+
+        series = data[variable]
+        acfOut = acf(series, nlags=(n+m))
+
+        # to asses the probability
+        seasonality_probability = np.zeros(n)
+        for i in range(n):
+
+            if i > 0:  # do not compute 0 all acf will have a maximum on
+
+                k = -1
+                x = acfOut[i]
+                local_max = True
+                while local_max:
+                    k += 1
+                    if k > int(i/2):
+                        break
+                    else:
+                        xki = acfOut[i - k - 1]
+                        xik = acfOut[i + k + 1]
+                        local_max = (x > xki and x > xik)
+                seasonality_probability[i] = min(np.sqrt(2 * k / i), 1)
+
+        season = pd.DataFrame()
+        season['probability'] = -np.sort(-seasonality_probability)
+        season['index'] = np.argsort(-seasonality_probability)
+
+        out = season[season.probability > probability_threshold]
+
+        dataOut = out.to_json(orient='records')
+
+        # Generates the written response
+        outL = out.shape[0]
+        introduction = 'Analysis have found ' + str(outL) + ' significant Seasonalities'
+        bulletPoints = []
+        for i in range(outL):
+            bulletPoints.append('Lag ' + str(out['index'][i]) + ' with probability: ' + str(out['probability'][i]))
+        postData = "Note that Multiple seasonalities must be ignored, they reinforce the first value"
+
+        writtenResponse = {
+            'introduction': introduction,
+            'bulletPoints': bulletPoints,
+            'postData': postData
+        }
+
+        # Final output JSON
+        outJson = {
+            'writtenResponse': [writtenResponse],
+            'data': dataOut
+        }
+
+        return Response(outJson)
+
--- anomaly_API/urls.py
+++ anomaly_API/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from . import SARIMA
 from . import WGAUSS
 from . import views
+from .analysis import seasonality
 
 urlpatterns = [
     path('SARIMA', SARIMA.AD_SARIMA.as_view(), name='SARIMA'),
     path('ADWindowedStats', WGAUSS.ADWindowedStats.as_view(), name='ADWindowedStats'),
     path('read-data', views.ReadUploadedData.as_view(), name='read-data'),
-    path('import-data', views.ImportData.as_view(), name='import-data')
+    path('import-data', views.ImportData.as_view(), name='import-data'),
+    path('analysis/CheckSeasonality', seasonality.CheckSeasonality.as_view(), name='CheckSeasonality')
 ]
