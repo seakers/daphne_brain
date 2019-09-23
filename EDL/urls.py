a0e00b83d91f37e219fa906d25a45121b302fcc7
Antoni Viros <ani300@gmail.com>
2019-09-23 14:55:09 -0500

    Third pass at bugfixing

--- EDL/urls.py
+++ EDL/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path, include
 
-from daphne_brain import settings
-
-if "EDL" in settings.ACTIVE_MODULES:
-    urlpatterns = [
-        path('data/', include('EDL.data.urls')),
-        path('dialogue/', include('EDL.dialogue.urls')),
-    ]
+urlpatterns = [
+    path('data/', include('EDL.data.urls')),
+    path('dialogue/', include('EDL.dialogue.urls')),
+]
