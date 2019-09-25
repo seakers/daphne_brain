e888e0927c965a4d74d41bcfaf3f327e4a32babb
Oscar Balcells Quintana <oscar.balcells01@gmail.com>
2019-09-25 13:47:35 -0500

    added an example question

--- AT/dialogue/data_extractors.py
+++ AT/dialogue/data_extractors.py
@@ -1,15 +1,1 @@
-import pandas as pd
-
-def extract_telemetry_feed(processed_question, number_of_features, context):
-    filename = 'C:/Users/oscar/Desktop/SEAK LAB/Telemetry feed/telemetry_feed.csv'
-    telemetry_feed = pd.read_csv(filename)
-    return [telemetry_feed]
-
-def extract_telemetry_feed_thresholds(processed_question, number_of_features, context):
-    filename = 'C:/Users/oscar/Desktop/SEAK LAB/Telemetry feed/telemetry_feed_thresholds.csv'
-    telemetry_feed_thresholds = pd.read_csv(filename)
-    return [telemetry_feed_thresholds]
-
 extract_function = {}
-extract_function["telemetry_feed"] = extract_telemetry_feed
-extract_function["telemetry_feed_thresholds"] = extract_telemetry_feed_thresholds
