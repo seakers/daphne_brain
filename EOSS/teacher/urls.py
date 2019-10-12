e2cdc4870b08620545f0d987430294ebb0251ccd
Gabe Apaza <gapaza@tamu.edu>
2019-10-12 12:00:01 -0500

    teacher agent urls

--- EOSS/teacher/urls.py
+++ EOSS/teacher/urls.py
@@ -3,14 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    # --> Subject Getters
     path('get-subject-features', views.GetSubjectFeatures.as_view()),
-
-
     path('get-subject-sensitivities', views.GetSubjectSensitivities.as_view()),
-
-
     path('get-subject-objective-space', views.GetSubjectObjectiveSpace.as_view()),
-
-
     path('get-subject-design-space', views.GetSubjectDesignSpace.as_view())
 ]
