daf82e97437153e0e1a109b4d3c654924f1cfb67
Antoni Viros <ani300@gmail.com>
2019-12-06 13:55:21 -0600

    fix small bugs

--- AT/urls.py
+++ AT/urls.py
@@ -4,7 +4,7 @@ from AT import views
 
 urlpatterns = [
     path('dialogue/', include('AT.dialogue.urls')),
-    path('recommendation/', include('AT.recommendation.urls'))
+    path('recommendation/', include('AT.recommendation.urls')),
 
     path('simulate', views.SimulateTelemetry.as_view(), name='SimulateTelemetry'),
     path('stop', views.StopTelemetry.as_view(), name='StopTelemetry'),
