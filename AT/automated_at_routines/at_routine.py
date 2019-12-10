2aa4dd4e51cbcb2164ba340d64b2de154f771813
Oscar Balcells Quintana <oscar.balcells01@gmail.com>
2019-12-10 15:26:37 -0600

    Solving issues prior to master merge. Merged the detection and diagnosis threads.

--- AT/automated_at_routines/at_routine.py
+++ AT/automated_at_routines/at_routine.py
@@ -7,7 +7,7 @@ def compute_zone(value, info):
     This function returns an integer depending on the threshold zone on which a value is located.
     """
 
-    zone = 2
+    zone = 0
     if value <= info['low_critic_threshold']:
         zone = -2
     elif value <= info['low_warning_threshold']:
@@ -16,6 +16,8 @@ def compute_zone(value, info):
         zone = 0
     elif value <= info['high_critic_threshold']:
         zone = 1
+    else:
+        zone = 2
 
     return zone
 
@@ -37,6 +39,9 @@ def threshold_check(value, info):
         message = 'above high warning threshold'
     elif zone == 2:
         message = 'above high critical threshold'
+    else:
+        print('Invalid zone value')
+        raise
 
     return message
 
@@ -52,7 +57,7 @@ def forecast_check(trace, info):
     base_zone = compute_zone(last_point, info)
 
     # AR model fitting and prediction
-    forecast_span = 120
+    forecast_span = 120  # number of points to forecast (its value in seconds depends on the simulation configuration)
     start_index = len(trace) - 1
     end_index = start_index + forecast_span
     model = ar_model.AR(endog=trace)
@@ -75,7 +80,9 @@ def forecast_check(trace, info):
                 message = 'high warning zone in ' + str(second) + ' seconds'
             elif zone == 2:
                 message = 'high critical zone in ' + str(second) + ' seconds'
-
+            else:
+                print('Invalid zone value')
+                raise
             break
 
         second += 1
@@ -114,25 +121,29 @@ def build_message(window):
     return messages
 
 
-def anomaly_detection_routine(hub_to_ad, ad_to_diag):
+def anomaly_treatment_routine(hub_to_at, at_to_hub):
     print('AD thread started')
 
     keep_alive = True
     while keep_alive:
-        while not hub_to_ad.empty():
-            signal = hub_to_ad.get()
+        while not hub_to_at.empty():
+            signal = hub_to_at.get()
             if signal['type'] == 'stop':
                 keep_alive = False
-                ad_to_diag.put({'type': 'stop', 'content': ''})
+                # at_to_hub.put({'type': 'stop', 'content': ''})
             elif signal['type'] == 'window':
                 # To prevent the anomaly detection thread from falling behind the telemetry feed, empty the queue and
                 # only process the last received window
-                if hub_to_ad.empty():
+                if hub_to_at.empty():
                     window = signal['content']
-                    frontend_ad_message = build_message(window)
-                    ad_to_diag.put({'type': 'anomaly_list', 'content': frontend_ad_message})
+                    anomaly_list = build_message(window)
+
+                    # Trigger diagnosis here
+
+                    # Send message to frontend
+                    at_to_hub.put({'type': 'diagnosed_anomalies', 'content': anomaly_list})
 
         time.sleep(0.5)
 
-    print('Anomaly detection thread stopped.')
-    return None
+    print('Anomaly treatment thread stopped.')
+    return
--- AT/queue_objects.py
+++ AT/queue_objects.py
@@ -3,6 +3,5 @@ from queue import Queue
 frontend_to_hub_queue = Queue()
 simulator_to_hub_queue = Queue()
 hub_to_simulator_queue = Queue()
-hub_to_ad_queue = Queue()
-ad_to_diagnosis_queue = Queue()
-diagnosis_to_hub_queue = Queue()
+hub_to_at_queue = Queue()
+at_to_hub_queue = Queue()
