edb04413683fe0f32c4d73e47f0f51f8e7c952ba
Prachi <prachi.dutta@tamu.edu>
2020-01-17 12:08:29 -0600

    sECLSS integration

--- AT/urls.py
+++ AT/urls.py
@@ -5,8 +5,8 @@ from AT import views
 urlpatterns = [
     path('dialogue/', include('AT.dialogue.urls')),
     path('recommendation/', include('AT.recommendation.urls')),
-
     path('simulate', views.SimulateTelemetry.as_view(), name='SimulateTelemetry'),
     path('stop', views.StopTelemetry.as_view(), name='StopTelemetry'),
-    path('receiveSeclssFeed', views.SeclssFeed.as_view(), name='SeclssFeed')
+    path('startSeclssFeed', views.StartSeclssFeed.as_view(), name='startSeclssFeed'),
+    path('receiveSeclssFeed', views.SeclssFeed.as_view(), name='receiveSeclssFeed')
 ]
