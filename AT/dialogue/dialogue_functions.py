ff8671def1648bb03ba82898e0e33c017c686884
michaelcmcc1 <michaelcmcc1@tamu.edu>
2020-01-22 22:22:47 -0600

    Implementation of neo4j diagnosis function, test script included

--- AT/dialogue/dialogue_functions.py
+++ AT/dialogue/dialogue_functions.py
@@ -1,1 +1,2 @@
+import AT.diagnosis.dialogue_functions as diagnosis
 import AT.recommendation.dialogue_functions as recommendation
--- AT/neo4j_test.py
+++ AT/neo4j_test.py
@@ -33,3 +33,45 @@ query = "match (p:Procedure)-[:Has]-(s:Step) where p.pNumber=117 return s.Action
 result = session.run(query)
 steps = [node[0] for node in result]
 print(steps)
+
+# Diagnose anomaly RWGSR malfunction
+'''
+query =  MATCH (m1:Measurement)-[r1:Exceeds_LWL]->(g:Anomaly)
+         MATCH (m2:Measurement)-[r2:Exceeds_UWL]->(g:Anomaly)
+         MATCH (m3:Measurement)-[r3:Exceeds_UWL]->(g:Anomaly)
+         WHERE m1.Name='ppO2' and m2.Name='ppCO2' and m3.Name='Water Level'
+         RETURN DISTINCT g.Title
+'''
+
+print()
+
+# Function that can take the intersection of multiple symptom queries
+# Fabricate detection skill output (list of dictionaries)
+symptom1, symptom2, symptom3 = {}, {}, {}
+symptom1['measurement'], symptom1['relationship'] = 'ppO2', 'Exceeds_LWL'
+symptom2['measurement'], symptom2['relationship'] = 'ppCO2', 'Exceeds_UWL'
+symptom3['measurement'], symptom3['relationship'] = 'Water Level', 'Exceeds_UWL'
+symptoms = [symptom1, symptom2, symptom3]
+
+def diagnose_symptoms_neo4j(symptoms):
+    # setup neo4j database connection
+    driver = GraphDatabase.driver("bolt://13.58.54.49:7687", auth=basic_auth("neo4j", "goSEAKers!"))
+    session = driver.session()
+
+    # build the query based on the symptoms list
+    query = ''
+    for id, symp in enumerate(symptoms):
+        query = query + 'MATCH (m' + str(id) + ':Measurement)-[r' + str(id) + ':' + symp['relationship'] + ']->(g:Anomaly) '
+    query = query + 'WHERE '
+    for id, symp in enumerate(symptoms):
+        if ((id + 1) < len(symptoms)):
+            query = query + 'm' + str(id) + '.Name=\'' + symp['measurement'] + '\' and '
+        else:
+            query = query + 'm' + str(id) + '.Name=\'' + symp['measurement'] + '\' RETURN DISTINCT g.Title'
+
+    # query the database
+    result = session.run(query)
+    diagnosis = [node[0] for node in result]
+    return diagnosis
+
+print(diagnose_symptoms_neo4j(symptoms))
