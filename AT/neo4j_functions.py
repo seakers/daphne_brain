5d7ef9c53f65a2cf3ea1516f0fad00e050a3c418
michaelcmcc1 <michaelcmcc1@tamu.edu>
2020-02-04 12:33:28 -0600

    Finished procedure and steps fetching functions

--- AT/neo4j_functions.py
+++ AT/neo4j_functions.py
@@ -25,6 +25,7 @@ def diagnose_symptoms_neo4j(symptoms, session):
         else:
             query = query + 'm' + str(id) + '.Name=\'' + symp['measurement'] + '\' RETURN DISTINCT g.Title'
 
+    print(query)
     # query the database
     result = session.run(query)
     diagnosis = [node[0] for node in result]
@@ -36,12 +37,14 @@ print(diagnose_symptoms_neo4j(symptoms, session))
 def get_related_procedures(anomaly, session):
     query = ''
     if type(anomaly) is list:
-        query = query + 'MATCH (a:Anomaly)-[r:Solution]->(p:Procedure) WHERE '
+        for id, anom in enumerate(anomaly):
+            query = query + 'MATCH (a' + str(id) + ':Anomaly)-[r' + str(id) + ':Solution]->(p:Procedure) '
+        query = query + 'WHERE '
         for id, anom in enumerate(anomaly):
             if ((id + 1) < len(anomaly)):
-                query = query + 'a.Title=\'' + anom + '\' and '
+                query = query + 'a' + str(id) + '.Title=\'' + anom + '\' and '
             else:
-                query = query + 'a.Title=\'' + anom + '\' RETURN DISTINCT p.Title'
+                query = query + 'a' + str(id) + '.Title=\'' + anom + '\' RETURN DISTINCT p.Title'
     else:
         query = query + 'MATCH (a:Anomaly)-[r:Solution]->(p:Procedure) WHERE a.Title=\'' + str(anomaly) + '\' RETURN DISTINCT p.Title'
 
@@ -54,14 +57,41 @@ def get_related_procedures(anomaly, session):
         return procedures
 
 print(get_related_procedures('CDRA Failure', session))
-print(get_related_procedures(['CDRA Failure', 'N2 Tank Burst'], session))
+print(get_related_procedures(['CDRA Failure', 'Excess CO2 in Cabin'], session))
+
+# Function that can take a specific procedure and return all steps, substeps, and subsubsteps as an ordered list
+def get_procedure_steps(procedure, session, detail=3):
+    # detail denotes the level of steps to return. (1->steps, 2->steps&substeps, 3->steps&substeps&subsubsteps)
+    if (detail == 1):
+        #Return only highest level steps
+        query1 = 'MATCH(p:Procedure)-[r:Has]->(s) WHERE p.Title=\'' + procedure + '\' and (s:Step) RETURN s.Title ORDER BY s.Step, s.SubStep, s.SubSubStep, s.Note'
+        query2 = 'MATCH(p:Procedure)-[r:Has]->(s) WHERE p.Title=\'' + procedure + '\' and (s:Step) RETURN s.Action ORDER BY s.Step, s.SubStep, s.SubSubStep, s.Note'
+    elif (detail == 2):
+        #Return Steps and substeps
+        query1 = 'MATCH(p:Procedure)-[r:Has]->(s) WHERE p.Title=\'' + procedure + '\' and (s:Step OR s:SubStep) RETURN s.Title ORDER BY s.Step, s.SubStep, s.SubSubStep, s.Note'
+        query2 = 'MATCH(p:Procedure)-[r:Has]->(s) WHERE p.Title=\'' + procedure + '\' and (s:Step OR s:SubStep) RETURN s.Action ORDER BY s.Step, s.SubStep, s.SubSubStep, s.Note'
+    else:
+        #Return all steps, substeps, and subsubsteps
+        query1 = 'MATCH(p:Procedure)-[r:Has]->(s) WHERE p.Title=\'' + procedure + '\' RETURN s.Title ORDER BY s.Step, s.SubStep, s.SubSubStep, s.Note'
+        query2 = 'MATCH(p:Procedure)-[r:Has]->(s) WHERE p.Title=\'' + procedure + '\' RETURN s.Action ORDER BY s.Step, s.SubStep, s.SubSubStep, s.Note'
+
+    steps = []
+    result1 = session.run(query1)
+    step_titles = [node[0] for node in result1]
+    result2 = session.run(query2)
+    step_actions = [node[0] for node in result2]
+    for id, step in enumerate(step_titles):
+        steps.append(step_titles[id] + ' - ' + step_actions[id])
+
+    if not steps:
+        return None
+    else:
+        return steps
+
+print(get_procedure_steps('Zeolite Filter Swapout', session, 3))
+
 
 '''
-# Function that can take a specific procedure and return all steps, substeps, and subsubsteps as an ordered list
-def get_procedure_steps(procedure, detail=3, session):
-    # detail denotes the level of steps to return. (1->steps, 2->steps&substeps, 3->steps&substeps&subsubsteps)
-    return steps
-
 # Function that returns the equipment required for a procedure or list of procedures
 def get_procedure_equipment(procedure, session):
     return equipment
