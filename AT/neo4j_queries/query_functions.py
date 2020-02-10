f2236770e8bdf168ff10e5e77f6b8d19125989b5
Oscar Balcells Quintana <oscar.balcells01@gmail.com>
2020-02-09 18:52:10 -0600

    Added more questions.

--- AT/neo4j_queries/query_functions.py
+++ AT/neo4j_queries/query_functions.py
@@ -98,6 +98,60 @@ def retrieve_risks_from_anomaly(anomaly_name):
     return risks_list
 
 
+def retrieve_affected_subsystems_from_anomaly(anomaly_name):
+    # Setup neo4j database connection
+    driver = GraphDatabase.driver("bolt://13.58.54.49:7687", auth=basic_auth("neo4j", "goSEAKers!"))
+    session = driver.session()
+
+    # Build and send the query
+    query = "MATCH (a:Anomaly)-[:Affects]-(s:SubSystem) WHERE a.Title='" + anomaly_name + "' RETURN DISTINCT s.Title"
+    result = session.run(query)
+
+    # Parse the result
+    subsystems_list = []
+    for item in result:
+        subsystems_list.append(item[0])
+
+    return subsystems_list
+
+
+def retrieve_symptoms_from_anomaly(anomaly_name):
+    # Setup neo4j database connection
+    driver = GraphDatabase.driver("bolt://13.58.54.49:7687", auth=basic_auth("neo4j", "goSEAKers!"))
+    session = driver.session()
+
+    # Build and send the query to obtain the affected measurements that exceed the UWL
+    query_uwl = "MATCH (a:Anomaly)-[r:Exceeds_UWL]-(m:Measurement) WHERE a.Title='" + anomaly_name +\
+                "' RETURN DISTINCT m.Name"
+    result_uwl = session.run(query_uwl)
+
+    # Parse the result
+    symptoms_list_uwl = []
+    for item in result_uwl:
+        symptoms_list_uwl.append(item[0])
+
+    # Build and send the query to obtain the affected measurements that exceed the UWL
+    query_lwl = "MATCH (a:Anomaly)-[r:Exceeds_LWL]-(m:Measurement) WHERE a.Title='" + anomaly_name + \
+                "' RETURN DISTINCT m.Name"
+    result_lwl = session.run(query_lwl)
+
+    # Parse the result
+    symptoms_list_lwl = []
+    for item in result_lwl:
+        symptoms_list_lwl.append(item[0])
+
+    # Build the output (making the relationship explicit)
+    symptoms_list = []
+    for measurement in symptoms_list_lwl:
+        symptom = {'measurement': measurement, 'relationship': 'Lower Warning Limit'}
+        symptoms_list.append(symptom)
+    for measurement in symptoms_list_uwl:
+        symptom = {'measurement': measurement, 'relationship': 'Upper Warning Limit'}
+        symptoms_list.append(symptom)
+
+    return symptoms_list
+
+
 def retrieve_thresholds_from_measurement(measurement_name):
     # Setup neo4j database connection
     driver = GraphDatabase.driver("bolt://13.58.54.49:7687", auth=basic_auth("neo4j", "goSEAKers!"))
