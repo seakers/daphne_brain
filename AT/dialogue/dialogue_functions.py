9bc4cdda695e94e59ce2372c9574a9accb083fe2
Oscar Balcells Quintana <oscar.balcells01@gmail.com>
2020-02-11 14:57:05 -0600

    Implemented all 'doable' questions (without context).

--- AT/dialogue/dialogue_functions.py
+++ AT/dialogue/dialogue_functions.py
@@ -8,7 +8,10 @@ from AT.neo4j_queries.query_functions import retrieve_risks_from_anomaly
 from AT.neo4j_queries.query_functions import retrieve_symptoms_from_anomaly
 from AT.neo4j_queries.query_functions import retrieve_procedures_from_anomaly
 from AT.neo4j_queries.query_functions import retrieve_affected_subsystems_from_anomaly
+from AT.neo4j_queries.query_functions import retrieve_affected_components_from_procedure
+from AT.neo4j_queries.query_functions import retrieve_time_from_procedure
 from AT.dialogue.data_helpers import last_measurement_value_from_context
+from AT.dialogue.data_helpers import pdf_link_from_procedure
 
 
 def get_measurement_current_value(measurement, context):
@@ -80,19 +83,55 @@ def get_anomaly_symptoms(anomaly):
     return symptoms
 
 
-def get_anomaly_procedures(anomaly):
+def get_procedure_affected_components(procedure):
     # Query the neo4j graph for the anomaly risks
+    components = retrieve_affected_components_from_procedure(procedure)
+
+    return components
+
+
+def get_anomaly_procedures(anomaly):
+    # Query the neo4j graph for the anomaly procedures
     procedures = retrieve_procedures_from_anomaly(anomaly)
     procedure_info_list = []
     for procedure in procedures:
-        procedure_info = {'procedure_name': procedure, 'procedure_link': 'EMPTYLINK'}
+        pdf_link = pdf_link_from_procedure(procedure)
+        procedure_info = {'procedure_name': procedure, 'pdf_link': pdf_link}
         procedure_info_list.append(procedure_info)
-
     return procedure_info_list
+
+
+def get_anomaly_etr(anomaly):
+    # Query the neo4j graph for the anomaly procedures
+    procedures = retrieve_procedures_from_anomaly(anomaly)
+    total_time = 0
+    procedure_info_list = []
+    for procedure in procedures:
+        procedure_time = retrieve_time_from_procedure(procedure)
+        total_time += procedure_time
+        procedure_info = {'procedure_name': procedure, 'procedure_time': procedure_time}
+        procedure_info_list.append(procedure_info)
+    total_procedure_info = {'procedure_name': 'TOTAL TIME', 'procedure_time': total_time}
+    procedure_info_list.append(total_procedure_info)
+    return procedure_info_list
+
+
+def get_procedure_etr(procedure_name):
+    # Query the neo4j graph for the procedure etr
+    procedures_time = retrieve_time_from_procedure(procedure_name)
+    procedure_info = {'procedure_name': procedure_name, 'procedure_time': procedures_time}
+    return procedure_info
+
+
+def get_procedure_pdf(procedure):
+    # Query the neo4j graph for the anomaly risks
+    pdf_link = pdf_link_from_procedure(procedure)
+    procedure_info = {'procedure_name': procedure, 'pdf_link': pdf_link}
+    return procedure_info
+
 
 def get_anomaly_affected_subsystem(anomaly):
     # Query the neo4j graph for the anomaly risks
     subsystems = retrieve_affected_subsystems_from_anomaly(anomaly)
-    print(subsystems)
 
     return subsystems
