01cbb75cd5d07a46cc77969657bbad57261da99d
Oscar Balcells Quintana <oscar.balcells01@gmail.com>
2020-02-13 15:11:33 -0600

    Modified at context.

--- AT/models.py
+++ AT/models.py
@@ -8,10 +8,24 @@ from daphne_context.models import UserInformation, DialogueContext
 class ATContext(models.Model):
     user_information = models.OneToOneField(UserInformation, on_delete=models.CASCADE)
 
-    current_anomaly = models.CharField(max_length=80)
-    current_step = models.IntegerField()
+    # In order to support object types, some of the following context variables are defined as TextFields. The variables
+    # are then stored as a json string and parsed when required. Not optimal, could be done better in the future.
+
+    # For the anomalies, measurements and procedures, a "selected" and "recent" context variable is defined. This allows
+    # to ask questions such as "What are the risks of THIS anomaly?" from the chat box. A proper method is then defined
+    # to retrieve the meaning of the "THIS" pointer.
+    selected_anomalies = models.TextField(default='')
+    recent_anomalies = models.TextField(default='')
+    selected_measurements = models.TextField(default='')
+    recent_measurements = models.TextField(default='')
+    selected_procedures = models.TextField(default='')
+    recent_procedures = models.TextField(default='')
+
+    # Current step context variable (to be defined)
+    current_procedure_step = models.TextField(default='')
+
+    # Current telemetry values context variable
     current_telemetry_values = models.TextField(default='')
-    current_telemetry_info = models.TextField(default='')
 
 
 class ATContextSerializer(serializers.ModelSerializer):
