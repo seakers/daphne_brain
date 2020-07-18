54077c37dc09f9a1fc56a01c7837e365a097c664
Kyle York <kyleyork2023@tamu.edu>
2020-07-18 16:52:55 -0500

    Extend fake telemetry to groups. Create four fake telemetry threads.

--- AT/automated_at_routines/hub_routine.py
+++ AT/automated_at_routines/hub_routine.py
@@ -3,7 +3,6 @@ import time
 import redis
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
-import AT.global_objects as global_obj
 
 from auth_API.helpers import get_or_create_user_information
 
@@ -17,8 +16,10 @@ def update_channel_layer_and_name(signal):
     return channel_layer, channel_name
 
 
-def hub_routine(front_to_hub, sEclss_to_hub, Sim_to_hub, hub_to_sEclss, hub_to_Sim, hub_to_sEclss_at, hub_to_Sim_at,
-                sEclss_at_to_hub, Sim_at_to_hub, request):
+def hub_routine(front_to_hub, sEclss_to_hub, sim_to_hub_one, sim_to_hub_two, sim_to_hub_three, sim_to_hub_four,
+                hub_to_sEclss, hub_to_sim_one, hub_to_sim_two, hub_to_sim_three, hub_to_sim_four, hub_to_sEclss_at,
+                hub_to_sim_at_one, hub_to_sim_at_two, hub_to_sim_at_three, hub_to_sim_at_four,sEclss_at_to_hub,
+                sim_at_to_hub_one, sim_at_to_hub_two, sim_at_to_hub_three, sim_at_to_hub_four, request):
     # Get the user information and channel layer
     user_info = get_or_create_user_information(request.session, request.user, 'AT')
     channel_layer = get_channel_layer()
@@ -34,6 +35,16 @@ def hub_routine(front_to_hub, sEclss_to_hub, Sim_to_hub, hub_to_sEclss, hub_to_S
     # Initialize the ping routine timer
     timer_start = time.time()
 
+    # Fake telemetry users
+    channel_layer_fake_one = None
+    channel_layer_fake_two = None
+    channel_layer_fake_three = None
+    channel_layer_fake_four = None
+    channel_name_fake_one = None
+    channel_name_fake_two = None
+    channel_name_fake_three = None
+    channel_name_fake_four = None
+
     # Start the hub routine
     while time_since_last_ping < life_limit:
         # Check the frontend input queue
@@ -42,18 +53,41 @@ def hub_routine(front_to_hub, sEclss_to_hub, Sim_to_hub, hub_to_sEclss, hub_to_S
             print('New message from the frontend: ' + signal['type'])
             if signal['type'] == 'stop_real_telemetry':
                 hub_to_sEclss.put({"type": "stop", "content": None})
-            elif signal['type'] == 'stop_fake_telemetry':
-                hub_to_Sim.put({"type": "stop", "content": None})
+            elif signal['type'] == 'stop_fake_telemetry_one':
+                hub_to_sim_one.put({"type": "stop", "content": None})
+                channel_layer_fake_one = None
+                channel_name_fake_one = None
+            elif signal['type'] == 'stop_fake_telemetry_two':
+                hub_to_sim_two.put({"type": "stop", "content": None})
+                channel_layer_fake_two = None
+                channel_name_fake_two = None
+            elif signal['type'] == 'stop_fake_telemetry_three':
+                hub_to_sim_three.put({"type": "stop", "content": None})
+                channel_layer_fake_three = None
+                channel_name_fake_three = None
+            elif signal['type'] == 'stop_fake_telemetry_four':
+                hub_to_sim_four.put({"type": "stop", "content": None})
+                channel_layer_fake_four = None
+                channel_name_fake_four = None
             elif signal['type'] == 'ping':
                 timer_start = time.time()
                 hub_to_sEclss.put({"type": "ping", "content": None})
-                hub_to_Sim.put({"type": "ping", "content": None})
+                hub_to_sim_one.put({"type": "ping", "content": None})
                 hub_to_sEclss_at.put({"type": "ping", "content": None})
-                hub_to_Sim_at.put({"type": "ping", "content": None})
+                hub_to_sim_at_one.put({"type": "ping", "content": None})
             elif signal['type'] == 'get_real_telemetry_params':
                 hub_to_sEclss.put({"type": "get_real_telemetry_params", "content": None})
             elif signal['type'] == 'get_fake_telemetry_params':
-                hub_to_Sim.put({"type": "get_fake_telemetry_params", "content": None})
+                if signal['content'] == channel_name_fake_one:
+                    hub_to_sim_one.put({"type": "get_fake_telemetry_params", "content": None})
+                elif signal['content'] == channel_name_fake_two:
+                    hub_to_sim_two.put({"type": "get_fake_telemetry_params", "content": None})
+                elif signal['content'] == channel_name_fake_three:
+                    hub_to_sim_three.put({"type": "get_fake_telemetry_params", "content": None})
+                elif signal['content'] == channel_name_fake_four:
+                    hub_to_sim_four.put({"type": "get_fake_telemetry_params", "content": None})
+                else:
+                    print('User not assigned a fake telemetry')
             elif signal['type'] == 'ws_configuration_update':
                 # Update the user information and channel layer
                 channel_layer, channel_name = update_channel_layer_and_name(signal)
@@ -66,6 +100,18 @@ def hub_routine(front_to_hub, sEclss_to_hub, Sim_to_hub, hub_to_sEclss, hub_to_S
                     async_to_sync(channel_layer.group_add)("sEclss_group", channel_name)
                     r.sadd("seclss-group-users", channel_name)
                     print(f"Channel {channel_name} added to group")
+            elif signal['type'] == 'add_fake_telemetry_one':
+                channel_layer_fake_one = signal['channel_layer']
+                channel_name_fake_one = signal['channel_name']
+            elif signal['type'] == 'add_fake_telemetry_two':
+                channel_layer_fake_two = signal['channel_layer']
+                channel_name_fake_two = signal['channel_name']
+            elif signal['type'] == 'add_fake_telemetry_three':
+                channel_layer_fake_three = signal['channel_layer']
+                channel_name_fake_three = signal['channel_name']
+            elif signal['type'] == 'add_fake_telemetry_four':
+                channel_layer_fake_four = signal['channel_layer']
+                channel_name_fake_four = signal['channel_name']
 
         # Check sEclss input queue
         if not sEclss_to_hub.empty():
@@ -105,16 +151,16 @@ def hub_routine(front_to_hub, sEclss_to_hub, Sim_to_hub, hub_to_sEclss, hub_to_S
                            'content': content}
                 async_to_sync(channel_layer.group_send)("sEclss_group", command)
 
-        # Check the simulator input queue
-        if not Sim_to_hub.empty():
-            signal = Sim_to_hub.get()
+        # Check the simulator input queues
+        if not sim_to_hub_one.empty():
+            signal = sim_to_hub_one.get()
             if signal['type'] == 'window':
                 # Parse the signal
                 tf_window = signal['content']
 
                 # Send the telemetry feed window to the anomaly detection queue
                 last_window = {'type': 'window', 'content': tf_window}
-                hub_to_Sim_at.put(last_window)
+                hub_to_sim_at_one.put(last_window)
 
                 # Try to convert the dataframes to json format (this fails from time to time with no explanation)
                 json_values = ''
@@ -131,7 +177,8 @@ def hub_routine(front_to_hub, sEclss_to_hub, Sim_to_hub, hub_to_sEclss, hub_to_S
                                'info': json_info}
                     command = {'type': 'telemetry_update',
                                'content': content}
-                    async_to_sync(channel_layer.send)(channel_name, command)
+                    if channel_layer_fake_one is not None:
+                        async_to_sync(channel_layer_fake_one.send)(channel_name_fake_one, command)
             elif signal['type'] == 'initialize_telemetry':
                 # Parse the signal
                 tf_window = signal['content']
@@ -141,28 +188,167 @@ def hub_routine(front_to_hub, sEclss_to_hub, Sim_to_hub, hub_to_sEclss, hub_to_S
                 content = {'variables_names': tf_variables}
                 command = {'type': 'initialize_telemetry',
                            'content': content}
-                async_to_sync(channel_layer.send)(channel_name, command)
+                if channel_layer_fake_one is not None:
+                    async_to_sync(channel_layer_fake_one.send)(channel_name_fake_one, command)
+
+        if not sim_to_hub_two.empty():
+            signal = sim_to_hub_two.get()
+            if signal['type'] == 'window':
+                # Parse the signal
+                tf_window = signal['content']
+
+                # Send the telemetry feed window to the anomaly detection queue
+                last_window = {'type': 'window', 'content': tf_window}
+                hub_to_sim_at_two.put(last_window)
+
+                # Try to convert the dataframes to json format (this fails from time to time with no explanation)
+                json_values = ''
+                json_info = ''
+                try:
+                    json_values = tf_window['values'].to_json()
+                    json_info = tf_window['info'].to_json()
+                except:
+                    print('Dataframe conversion failed')
+
+                # If the conversion was successful, update and send the telemetry update command for the frontend
+                if json_values != '' and json_info != '':
+                    content = {'values': json_values,
+                               'info': json_info}
+                    command = {'type': 'telemetry_update',
+                               'content': content}
+                    if channel_layer_fake_two is not None:
+                        async_to_sync(channel_layer_fake_two.send)(channel_name_fake_two, command)
+            elif signal['type'] == 'initialize_telemetry':
+                # Parse the signal
+                tf_window = signal['content']
+
+                # Retrieve the telemetry feed variables and send an initialization command to the frontend
+                tf_variables = list(tf_window['info'].columns.values)
+                content = {'variables_names': tf_variables}
+                command = {'type': 'initialize_telemetry',
+                           'content': content}
+                if channel_layer_fake_two is not None:
+                    async_to_sync(channel_layer_fake_two.send)(channel_name_fake_two, command)
+
+        if not sim_to_hub_three.empty():
+            signal = sim_to_hub_three.get()
+            if signal['type'] == 'window':
+                # Parse the signal
+                tf_window = signal['content']
+
+                # Send the telemetry feed window to the anomaly detection queue
+                last_window = {'type': 'window', 'content': tf_window}
+                hub_to_sim_at_three.put(last_window)
+
+                # Try to convert the dataframes to json format (this fails from time to time with no explanation)
+                json_values = ''
+                json_info = ''
+                try:
+                    json_values = tf_window['values'].to_json()
+                    json_info = tf_window['info'].to_json()
+                except:
+                    print('Dataframe conversion failed')
+
+                # If the conversion was successful, update and send the telemetry update command for the frontend
+                if json_values != '' and json_info != '':
+                    content = {'values': json_values,
+                               'info': json_info}
+                    command = {'type': 'telemetry_update',
+                               'content': content}
+                    if channel_layer_fake_three is not None:
+                        async_to_sync(channel_layer_fake_three.send)(channel_name_fake_three, command)
+            elif signal['type'] == 'initialize_telemetry':
+                # Parse the signal
+                tf_window = signal['content']
+
+                # Retrieve the telemetry feed variables and send an initialization command to the frontend
+                tf_variables = list(tf_window['info'].columns.values)
+                content = {'variables_names': tf_variables}
+                command = {'type': 'initialize_telemetry',
+                           'content': content}
+                if channel_layer_fake_three is not None:
+                    async_to_sync(channel_layer_fake_three.send)(channel_name_fake_three, command)
+
+        if not sim_to_hub_four.empty():
+            signal = sim_to_hub_four.get()
+            if signal['type'] == 'window':
+                # Parse the signal
+                tf_window = signal['content']
+
+                # Send the telemetry feed window to the anomaly detection queue
+                last_window = {'type': 'window', 'content': tf_window}
+                hub_to_sim_at_four.put(last_window)
+
+                # Try to convert the dataframes to json format (this fails from time to time with no explanation)
+                json_values = ''
+                json_info = ''
+                try:
+                    json_values = tf_window['values'].to_json()
+                    json_info = tf_window['info'].to_json()
+                except:
+                    print('Dataframe conversion failed')
+
+                # If the conversion was successful, update and send the telemetry update command for the frontend
+                if json_values != '' and json_info != '':
+                    content = {'values': json_values,
+                               'info': json_info}
+                    command = {'type': 'telemetry_update',
+                               'content': content}
+                    if channel_layer_fake_four is not None:
+                        async_to_sync(channel_layer_fake_four.send)(channel_name_fake_four, command)
+            elif signal['type'] == 'initialize_telemetry':
+                # Parse the signal
+                tf_window = signal['content']
+
+                # Retrieve the telemetry feed variables and send an initialization command to the frontend
+                tf_variables = list(tf_window['info'].columns.values)
+                content = {'variables_names': tf_variables}
+                command = {'type': 'initialize_telemetry',
+                           'content': content}
+                if channel_layer_fake_four is not None:
+                    async_to_sync(channel_layer_fake_four.send)(channel_name_fake_four, command)
 
         # Check the anomaly treatment output queue for sEclss
         if not sEclss_at_to_hub.empty():
             signal = sEclss_at_to_hub.get()
             if signal['type'] == 'symptoms_report':
-                async_to_sync(channel_layer.group_send)("sEclss_group", signal)
+                async_to_sync(channel_layer.group_send)('sEclss_group', signal)
 
-        # Check the anomaly treatment output queue for simulated telemetry
-        if not Sim_at_to_hub.empty():
-            signal = Sim_at_to_hub.get()
+        # Check the anomaly treatment output queues for simulated telemetry
+        if not sim_at_to_hub_one.empty():
+            signal = sim_at_to_hub_one.get()
             if signal['type'] == 'symptoms_report':
-                async_to_sync(channel_layer.send)(channel_name, signal)
+                async_to_sync(channel_layer_fake_one.send)(channel_name_fake_one, signal)
+
+        if not sim_at_to_hub_two.empty():
+            signal = sim_at_to_hub_two.get()
+            if signal['type'] == 'symptoms_report':
+                async_to_sync(channel_layer_fake_two.send)(channel_name_fake_two, signal)
+
+        if not sim_at_to_hub_three.empty():
+            signal = sim_at_to_hub_three.get()
+            if signal['type'] == 'symptoms_report':
+                async_to_sync(channel_layer_fake_three.send)(channel_name_fake_three, signal)
+
+        if not sim_at_to_hub_four.empty():
+            signal = sim_at_to_hub_four.get()
+            if signal['type'] == 'symptoms_report':
+                async_to_sync(channel_layer_fake_four.send)(channel_name_fake_four, signal)
 
         # Update while loop condition and wait
         time_since_last_ping = time.time() - timer_start
         time.sleep(check_delay)
 
     hub_to_sEclss.put({'type': 'stop', 'content': ''})
-    hub_to_Sim.put({'type': 'stop', 'content': ''})
+    hub_to_sim_one.put({'type': 'stop', 'content': ''})
+    hub_to_sim_two.put({'type': 'stop', 'content': ''})
+    hub_to_sim_three.put({'type': 'stop', 'content': ''})
+    hub_to_sim_four.put({'type': 'stop', 'content': ''})
     hub_to_sEclss_at.put({'type': 'stop', 'content': ''})
-    hub_to_Sim_at.put({'type': 'stop', 'content': ''})
+    hub_to_sim_at_one.put({'type': 'stop', 'content': ''})
+    hub_to_sim_at_two.put({'type': 'stop', 'content': ''})
+    hub_to_sim_at_three.put({'type': 'stop', 'content': ''})
+    hub_to_sim_at_four.put({'type': 'stop', 'content': ''})
 
     # Clear the queues and print a stop message
     front_to_hub.queue.clear()
--- AT/global_objects.py
+++ AT/global_objects.py
@@ -3,18 +3,48 @@ from queue import Queue
 # Initialize threads
 hub_thread = None
 sEclss_thread = None
-Simulator_thread = None
 sEclss_at_thread = None
-Simulator_at_thread = None
+simulator_thread_one = None
+simulator_thread_two = None
+simulator_thread_three = None
+simulator_thread_four = None
+simulator_threads = [simulator_thread_one, simulator_thread_two, simulator_thread_three, simulator_thread_four]
+simulator_at_thread_one = None
+simulator_at_thread_two = None
+simulator_at_thread_three = None
+simulator_at_thread_four = None
+simulator_at_threads = [simulator_at_thread_one, simulator_at_thread_two, simulator_at_thread_three,
+                        simulator_at_thread_four]
 
 # Initialize queues
 frontend_to_hub_queue = Queue()
 sEclss_to_hub_queue = Queue()
-Simulator_to_hub_queue = Queue()
 hub_to_sEclss_queue = Queue()
-hub_to_Simulator_queue = Queue()
 hub_to_sEclss_at_queue = Queue()
-hub_to_Simulator_at_queue = Queue()
 sEclss_at_to_hub_queue = Queue()
-Simulator_at_to_hub_queue = Queue()
 server_to_sEclss_queue = Queue(maxsize=3600)
+simulator_to_hub_queue_one = Queue()
+simulator_to_hub_queue_two = Queue()
+simulator_to_hub_queue_three = Queue()
+simulator_to_hub_queue_four = Queue()
+simulator_to_hub_queues = [simulator_to_hub_queue_one, simulator_to_hub_queue_two, simulator_to_hub_queue_three,
+                           simulator_to_hub_queue_four]
+hub_to_simulator_queue_one = Queue()
+hub_to_simulator_queue_two = Queue()
+hub_to_simulator_queue_three = Queue()
+hub_to_simulator_queue_four = Queue()
+hub_to_simulator_queues = [hub_to_simulator_queue_one, hub_to_simulator_queue_two, hub_to_simulator_queue_three,
+                           hub_to_simulator_queue_four]
+hub_to_simulator_at_queue_one = Queue()
+hub_to_simulator_at_queue_two = Queue()
+hub_to_simulator_at_queue_three = Queue()
+hub_to_simulator_at_queue_four = Queue()
+hub_to_simulator_at_queues = [hub_to_simulator_at_queue_one, hub_to_simulator_at_queue_two, hub_to_simulator_queue_three,
+                              hub_to_simulator_at_queue_four]
+simulator_at_to_hub_queue_one = Queue()
+simulator_at_to_hub_queue_two = Queue()
+simulator_at_to_hub_queue_three = Queue()
+simulator_at_to_hub_queue_four = Queue()
+simulator_at_to_hub_queues = [simulator_at_to_hub_queue_one, simulator_at_to_hub_queue_two, simulator_at_to_hub_queue_three,
+                              simulator_at_to_hub_queue_four]
+
