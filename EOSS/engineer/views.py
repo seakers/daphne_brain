1791b3a4f19c3ce67557539cb909182fc97496d1
Ben <bgorr@tamu.edu>
2020-09-05 18:11:47 -0500

    First commit to scheduling branch

--- EOSS/engineer/views.py
+++ EOSS/engineer/views.py
@@ -10,13 +10,15 @@ from EOSS.data.problem_specific import assignation_problems, partition_problems
 from EOSS.vassar.api import VASSARClient
 from auth_API.helpers import get_or_create_user_information
 from EOSS.data.design_helpers import add_design
+from EOSS.historian.historian import Historian
+from EOSS.vassar.interface.ttypes import MissionMeasurements
 
 # Get an instance of a logger
 logger = logging.getLogger('EOSS.engineer')
 
 
 class GetOrbitList(APIView):
-    
+
     def post(self, request, format=None):
         try:
             # Start connection with VASSAR
@@ -25,11 +27,11 @@ class GetOrbitList(APIView):
             client = VASSARClient(port)
             client.start_connection()
             orbit_list = client.get_orbit_list(request.data['problem_name'])
-            
+
             # End the connection before return statement
             client.end_connection()
             return Response(orbit_list)
-        
+
         except TApplicationException as exc:
             logger.exception('Getting the orbit list failed')
             client.end_connection()
@@ -49,11 +51,11 @@ class GetInstrumentList(APIView):
             # Start connection with VASSAR
             client.start_connection()
             instrument_list = client.get_instrument_list(request.data['problem_name'])
-            
+
             # End the connection before return statement
             client.end_connection()
             return Response(instrument_list)
-        
+
         except TApplicationException as exc:
             logger.exception('Getting the instrument list failed')
             client.end_connection()
@@ -64,7 +66,7 @@ class GetInstrumentList(APIView):
 
 
 class EvaluateArchitecture(APIView):
-    
+
     def post(self, request, format=None):
         try:
             user_info = get_or_create_user_information(request.session, request.user, 'EOSS')
@@ -97,7 +99,7 @@ class EvaluateArchitecture(APIView):
             # End the connection before return statement
             client.end_connection()
             return Response(architecture)
-        
+
         except TApplicationException as exc:
             logger.exception('Evaluating an architecture failed')
             client.end_connection()
@@ -205,6 +207,150 @@ class GetArchDetails(APIView):
                 "explanation": str(exc)
             })
 
+
+class GetMultArchMeas(APIView):
+
+    def post(self, request, format=None):
+        try:
+            # Start VASSAR connection
+            user_info = get_or_create_user_information(request.session, request.user, 'EOSS')
+            port = user_info.eosscontext.vassar_port
+            client = VASSARClient(port)
+            client.start_connection()
+
+            # Get architectures
+            arch_string = request.data['arch_ids']
+            arch_list = arch_string.split(",")
+            problem = request.data['problem']
+            measurements = []
+            for arch_id in arch_list:
+                measurement = client.get_measurement_list(problem, user_info.eosscontext.design_set.get(id__exact=arch_id))
+                measurements.append(measurement)
+            client.end_connection()
+
+            return Response(measurements)
+        except TApplicationException as exc:
+            logger.exception('Exception when retrieving measurements from architectures.')
+            return Response({
+                "error": "Retrieving measurements from selected architectures failed.",
+                "explanation": str(exc)
+            })
+
+
+class GetMissions(APIView):
+
+    def post(self, request, format=None):
+        try:
+            historian = Historian()
+            missions = historian.get_all_missions()
+            return Response(missions)
+        except Exception:
+            logger.exception("Error getting missions.")
+            return Response("")
+
+
+class GetMissionMeasurements(APIView):
+
+    def post(self, request, format=None):
+        try:
+            historian = Historian()
+            mission_measurements = historian.get_all_mission_measurements()
+            return Response(mission_measurements)
+        except Exception:
+            logger.exception("Error getting mission measurements.")
+            return Response("")
+
+
+class GetSchedulingEval(APIView):
+    def post(self, request, format=None):
+        try:
+            # Start VASSAR connection
+            user_info = get_or_create_user_information(request.session, request.user, 'EOSS')
+            port = user_info.eosscontext.vassar_port
+            client = VASSARClient(port)
+            client.start_connection()
+            historian = Historian()
+            historical_mission_data = historian.get_all_mission_measurements()
+            arch_string = request.data['arch_ids']
+            arch_list = arch_string.split(",")
+            problem = request.data['problem']
+            mission_data = []
+            date_string = request.data['dates']
+            date_list = date_string.split(",")
+            count = 0
+            for arch_id in arch_list:
+                arch = user_info.eosscontext.design_set.get(id__exact=arch_id)
+                measurements = client.get_measurement_list(problem, arch)
+                start_date = int(date_list[count])
+                end_date = int(date_list[count + 1])
+                count = count + 2
+                mission_data.append(MissionMeasurements(measurements, start_date, end_date))
+            score = client.get_scheduling_eval(mission_data, historical_mission_data)
+            client.end_connection()
+            return Response(score)
+        except Exception:
+            logger.exception("Error evaluating scheduling architecture.")
+            return Response("")
+
+class StartSchedulingGA(APIView):
+    def post(self, request, format=None):
+        try:
+            # Start VASSAR connection
+            user_info = get_or_create_user_information(request.session, request.user, 'EOSS')
+            port = user_info.eosscontext.vassar_port
+            client = VASSARClient(port)
+            client.start_connection()
+            historian = Historian()
+            historical_mission_data = historian.get_all_mission_measurements()
+            arch_string = request.data['arch_ids']
+            arch_list = arch_string.split(",")
+            problem = request.data['problem']
+            arches = []
+            thrift_list = []
+            for arch_id in arch_list:
+                arch = user_info.eosscontext.design_set.get(id__exact=arch_id)
+                arches.append(arch)
+            score = client.start_scheduling_ga(problem, request.user.username, thrift_list, arches, historical_mission_data)
+            client.end_connection()
+            return Response(score)
+        except Exception:
+            logger.exception("Error evaluating scheduling architecture.")
+            return Response("")
+
+class StartEnumeration(APIView):
+    def post(self, request, format=None):
+        try:
+            # Start VASSAR connection
+            user_info = get_or_create_user_information(request.session, request.user, 'EOSS')
+            port = user_info.eosscontext.vassar_port
+            client = VASSARClient(port)
+            client.start_connection()
+            historian = Historian()
+            historical_mission_data = historian.get_all_mission_measurements()
+            arch_string = request.data['arch_ids']
+            arch_list = arch_string.split(",")
+            problem = request.data['problem']
+            arches = []
+            for arch_id in arch_list:
+                arch = user_info.eosscontext.design_set.get(id__exact=arch_id)
+                arches.append(arch)
+            score = client.start_enumeration(problem, arches, historical_mission_data)
+            client.end_connection()
+            return Response(score)
+        except Exception:
+            logger.exception("Error evaluating scheduling architecture.")
+            return Response("")
+
+class DataContinuityTable(APIView):
+    def post(self, request, format=None):
+        try:
+            # Start VASSAR connection
+            historian = Historian()
+            measurement_list, array = historian.data_continuity_table();
+            return Response(measurement_list, array)
+        except Exception:
+            logger.exception("Error evaluating scheduling architecture.")
+            return Response("")
 
 class GetSubobjectiveDetails(APIView):
 
