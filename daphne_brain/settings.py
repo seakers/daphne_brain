1469b085c94e336dbb04151800e88b28a9541595
prachichi <pd385@cornell.edu>
2021-01-18 09:45:20 -0600

    Added dev and prod servers to ALLOWED_HOSTS

--- daphne_brain/settings.py
+++ daphne_brain/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = 'aaaaa'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'www.selva-research.com', 'selva-research.engr.tamu.edu']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'www.selva-research.com', 'selva-research.engr.tamu.edu',
+                 'daphne-at-dev.selva-research.com', 'daphne-at.selva-research.com']
 
 USE_X_FORWARDED_HOST = True
 
