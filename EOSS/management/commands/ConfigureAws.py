0f144c88a1cf7e63b4bec651540e13dc9e249880
gapaza <gapaza@tamu.edu>
2021-02-03 21:00:31 -0600

    enabled task deregistration for testing

--- EOSS/management/commands/ConfigureAws.py
+++ EOSS/management/commands/ConfigureAws.py
@@ -11,13 +11,13 @@ from EOSS.aws.AutoScalingService import AutoScalingService
 class Command(BaseCommand):
     help = 'Configures aws for scalable daphne architecture'
 
-    def proceed_value(self):
-        response = input('\nWARNING: this command will wipe all AWS Queues and Task Definitions to create fresh ones. Type yes to proceed: ')
+    def user_input(self, yes_no_message):
+        response = input(yes_no_message)
         if response != 'yes':
             print('EXITING COMMAND')
-            return False
-        else:
             return True
+        else:
+            return False
 
     def pprint(self, to_print):
         pp = pprint.PrettyPrinter(indent=4)
@@ -32,7 +32,6 @@ class Command(BaseCommand):
         cluster_client = Cluster(dev=dev)
         cluster_client.remove_services()
 
-
         """
             Deregister all task definitions of task family 'evaluator'
         """
@@ -46,26 +45,35 @@ class Command(BaseCommand):
         queue_client.delete_all_eval_queues()
 
 
+    def create_queues(self, problems):
+        self.pprint(problems)
+        if not self.user_input('Queues and services will be created for each of these problems. Would you like to continue? (yes/no)'):
+            print('--- EXITING')
+            exit(0)
+        queue_client = EvalQueue(dev=False)
+        for problem in problems:
+            url = queue_client.create_eval_queue(problem['id'])
+            problem['queue_url'] = url
+        return problems
+
 
     def handle(self, *args, **options):
         print('---> CONFIGURING DAPHNE AWS ARCHITECTURE')
-        if not self.proceed_value():
+        if self.user_input('\nWARNING: this command will wipe all AWS Queues and Task Definitions to create fresh ones. Type yes to proceed: '):
             return
 
         # 1. Get problem ids
         graphql_client = GraphqlClient()
         problems = graphql_client.get_problems()
-        self.pprint(problems)
+
 
         # 2. Delete all cluster services, task definitions, and pre-existing eval queues
         self.remove_all_aws_eval_resources(dev=False)
 
+
         # 3. For each problem id, create an eval queue and store the url
-        # queue_client = EvalQueue(dev=False)
-        # for problem in problems:
-        #     url = queue_client.create_eval_queue(problem['id'])
-        #     problem['queue_url'] = url
-        # self.pprint(problems)
+        problems = self.create_queues(problems)
+
 
         # 4. For each problem, creating an auto scaling service
         # service_arns = []
