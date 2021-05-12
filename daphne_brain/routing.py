829f5d2b74949e757c99d56e97a16afaffb9fb04
prachichi <pd385@cornell.edu>
2021-05-11 23:57:48 -0500

    asgi django related errors removed

--- daphne_brain/routing.py
+++ daphne_brain/routing.py
@@ -1,8 +1,9 @@
+from channels.auth import AuthMiddlewareStack
 from django.urls import path
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 from daphne_brain import settings
 from experiment_at.consumers import ATExperimentConsumer
-
 if "EOSS" in settings.ACTIVE_MODULES:
     from EOSS.consumers import EOSSConsumer
 if "AT" in settings.ACTIVE_MODULES:
@@ -10,9 +11,7 @@ if "AT" in settings.ACTIVE_MODULES:
     # from AT.AD.algorithms.KNN import adaptiveKNN
     # from AT.AD.algorithms.I_Forest import iForest
     from AT.consumers import ATConsumer
-
 from experiment.consumers import ExperimentConsumer
-
 # The channel routing defines what connections get handled by what consumers,
 # selecting on either the connection type (ProtocolTypeRouter) or properties
 # of the connection's scope (like URLRouter, which looks at scope["path"])
@@ -20,15 +19,25 @@ from experiment.consumers import ExperimentConsumer
 
 ws_routes = []
 if "EOSS" in settings.ACTIVE_MODULES:
-    ws_routes.append(path('api/eoss/ws', EOSSConsumer.as_asgi()))
+    ws_routes.append(path('api/eoss/ws', EOSSConsumer))
 if "AT" in settings.ACTIVE_MODULES:
     ws_routes.extend([
-        path('api/at/ws', ATConsumer.as_asgi()),
-        path('api/at/experiment', ATExperimentConsumer.as_asgi()),
-        # path('api/anomaly/SARIMAX_AD', SARIMAX_AD.as_asgi()),
-        # path('api/anomaly/adaptiveKNN', adaptiveKNN.as_asgi()),
-        # path('api/anomaly/iForest', iForest.as_asgi()),
+        # path('api/at/SARIMAX_AD', SARIMAX_AD),
+        # path('api/at/adaptiveKNN', adaptiveKNN),
+        # path('api/at/iForest', iForest),
+        path('api/at/ws', ATConsumer),
+        path('api/at/experiment', ATExperimentConsumer)
     ])
 ws_routes.extend([
-    path('api/experiment', ExperimentConsumer.as_asgi()),
+    path('api/experiment', ExperimentConsumer),
 ])
+
+application = ProtocolTypeRouter({
+    # Route all WebSocket requests to our custom chat handler.
+    # We actually don't need the URLRouter here, but we've put it in for
+    # illustration. Also note the inclusion of the AuthMiddlewareStack to
+    # add users and sessions - see http://channels.readthedocs.io/en/latest/topics/authentication.html
+    'websocket': AuthMiddlewareStack(
+        URLRouter(ws_routes),
+    ),
+})
--- daphne_brain/settings.py
+++ daphne_brain/settings.py
@@ -164,7 +164,7 @@ CHANNEL_LAYERS = {
 }
 
 # ASGI_APPLICATION should be set to your outermost router
-ASGI_APPLICATION = 'daphne_brain.asgi.application'
+ASGI_APPLICATION = 'daphne_brain.routing.application'
 
 # Databases for Daphne
 ALCHEMY_DATABASE = {
