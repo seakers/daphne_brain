d916a1a40259c4e53c00362a169d2b2a68444d85
prachichi <pd385@cornell.edu>
2021-05-18 13:03:00 -0500

    if step/procedure numbers contain spaces then remove them

--- dialogue/qa_pipeline.py
+++ dialogue/qa_pipeline.py
@@ -2,6 +2,7 @@ import datetime
 import json
 import os
 import pickle
+import re
 from string import Template
 
 import numpy as np
@@ -115,101 +116,107 @@ def is_number(x):
 
 
 def convert_string_to_number(textnum, numwords=None):
-    if numwords is None:
-        numwords = {}
 
-    units = [
-        'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
-        'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
-        'sixteen', 'seventeen', 'eighteen', 'nineteen',
-    ]
-    tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
-    scales = ['hundred', 'thousand', 'million', 'billion', 'trillion']
-    ordinal_words = {'first': 1, 'second': 2, 'third': 3, 'fifth': 5, 'eighth': 8, 'ninth': 9, 'twelfth': 12}
-    ordinal_endings = [('ieth', 'y'), ('th', '')]
+    if re.search('[a-zA-Z]', textnum):
+        if numwords is None:
+            numwords = {}
 
-    if not numwords:
-        numwords['and'] = (1, 0)
-        for idx, word in enumerate(units): numwords[word] = (1, idx)
-        for idx, word in enumerate(tens): numwords[word] = (1, idx * 10)
-        for idx, word in enumerate(scales): numwords[word] = (10 ** (idx * 3 or 2), 0)
+        units = [
+            'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
+            'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
+            'sixteen', 'seventeen', 'eighteen', 'nineteen',
+        ]
+        tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+        scales = ['hundred', 'thousand', 'million', 'billion', 'trillion']
+        ordinal_words = {'first': 1, 'second': 2, 'third': 3, 'fifth': 5, 'eighth': 8, 'ninth': 9, 'twelfth': 12}
+        ordinal_endings = [('ieth', 'y'), ('th', '')]
 
-    textnum = textnum.replace('-', ' ')
+        if not numwords:
+            numwords['and'] = (1, 0)
+            for idx, word in enumerate(units): numwords[word] = (1, idx)
+            for idx, word in enumerate(tens): numwords[word] = (1, idx * 10)
+            for idx, word in enumerate(scales): numwords[word] = (10 ** (idx * 3 or 2), 0)
 
-    current = result = 0
-    curstring = ''
-    onnumber = False
-    lastunit = False
-    lastscale = False
+        textnum = textnum.replace('-', ' ')
 
-    def is_numword(x):
-        if is_number(x):
-            return True
-        if word in numwords:
-            return True
-        return False
+        current = result = 0
+        curstring = ''
+        onnumber = False
+        lastunit = False
+        lastscale = False
 
-    def from_numword(x):
-        if is_number(x):
-            scale = 0
-            increment = int(x.replace(',', ''))
-            return scale, increment
-        return numwords[x]
+        def is_numword(x):
+            if is_number(x):
+                return True
+            if word in numwords:
+                return True
+            return False
 
-    for word in textnum.split():
-        if word in ordinal_words:
-            scale, increment = (1, ordinal_words[word])
-            current = current * scale + increment
-            if scale > 100:
-                result += current
-                current = 0
-            onnumber = True
-            lastunit = False
-            lastscale = False
-        else:
-            for ending, replacement in ordinal_endings:
-                if word.endswith(ending):
-                    word = "%s%s" % (word[:-len(ending)], replacement)
+        def from_numword(x):
+            if is_number(x):
+                scale = 0
+                increment = int(x.replace(',', ''))
+                return scale, increment
+            return numwords[x]
 
-            if (not is_numword(word)) or (word == 'and' and not lastscale):
-                if onnumber:
-                    # Flush the current number we are building
-                    curstring += repr(result + current) + " "
-                curstring += word + " "
-                result = current = 0
-                onnumber = False
-                lastunit = False
-                lastscale = False
-            else:
-                scale, increment = from_numword(word)
-                onnumber = True
-
-                if lastunit and (word not in scales):
-                    # Assume this is part of a string of individual numbers to
-                    # be flushed, such as a zipcode "one two three four five"
-                    curstring += repr(result + current)
-                    result = current = 0
-
-                if scale > 1:
-                    current = max(1, current)
-
+        for word in textnum.split():
+            if word in ordinal_words:
+                scale, increment = (1, ordinal_words[word])
                 current = current * scale + increment
                 if scale > 100:
                     result += current
                     current = 0
-
-                lastscale = False
+                onnumber = True
                 lastunit = False
-                if word in scales:
-                    lastscale = True
-                elif word in units:
-                    lastunit = True
+                lastscale = False
+            else:
+                for ending, replacement in ordinal_endings:
+                    if word.endswith(ending):
+                        word = "%s%s" % (word[:-len(ending)], replacement)
 
-    if onnumber:
-        curstring += repr(result + current)
+                if (not is_numword(word)) or (word == 'and' and not lastscale):
+                    if onnumber:
+                        # Flush the current number we are building
+                        curstring += repr(result + current) + " "
+                    curstring += word + " "
+                    result = current = 0
+                    onnumber = False
+                    lastunit = False
+                    lastscale = False
+                else:
+                    scale, increment = from_numword(word)
+                    onnumber = True
 
-    # remove all whitespaces
-    curstring = curstring.replace(" ", "")
+                    if lastunit and (word not in scales):
+                        # Assume this is part of a string of individual numbers to
+                        # be flushed, such as a zipcode "one two three four five"
+                        curstring += repr(result + current)
+                        result = current = 0
+
+                    if scale > 1:
+                        current = max(1, current)
+
+                    current = current * scale + increment
+                    if scale > 100:
+                        result += current
+                        current = 0
+
+                    lastscale = False
+                    lastunit = False
+                    if word in scales:
+                        lastscale = True
+                    elif word in units:
+                        lastunit = True
+
+        if onnumber:
+            curstring += repr(result + current)
+
+        # remove all whitespaces
+        curstring = curstring.replace(" ", "")
+
+    else:
+        # remove all whitespaces
+        curstring = textnum.replace(" ", "")
 
     return curstring
 
