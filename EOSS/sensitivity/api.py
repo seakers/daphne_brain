c90d5d36a6f59bfa827399c5d35e2096c64a2357
gapaza <gapaza@tamu.edu>
2021-11-03 13:06:23 -0500

    updates

--- EOSS/sensitivity/api.py
+++ EOSS/sensitivity/api.py
@@ -272,10 +272,10 @@ class SensitivityClient:
         sampling = AssigningSampling(self.instruments, self.orbits)
 
         # 3. Calculate orbit sensitivities
-        self.calculate_orbit_sensitivities(sampling, problem_name)
+        # self.calculate_orbit_sensitivities(sampling, problem_name)
 
         # 4. Create new dataset
-        self.new_dataset()
+        # self.new_dataset()
 
         # 5. Calculate instrument sensitivities
         self.calculate_instrument_sensitivities(sampling, problem_name)
