daf319b78eb448450dfdbe99fdb3e45d89121dd0
gabe <gapaza@tamu.edu>
2022-01-02 11:05:46 -0500

    fixed mycroft consumer connection / made the consumer async

--- mycroft/views.py
+++ mycroft/views.py
@@ -7,10 +7,15 @@ from auth_API.helpers import get_or_create_user_information
 class CheckConnection(APIView):
 
     def post(self, request, format=None):
-        # user_info = get_or_create_user_information(request.session, request.user, request.data['problem'])
+
+        # --> 1. Get connection status and id
         user_info = get_or_create_user_information(request.session, request.user, 'EOSS')
-        if user_info.mycroft_connection is False:
-            return Response({"connection": "false", "access_token": user_info.user.mycroftuser.mycroft_session})
+        conn_status = user_info.mycroft_connection
+        conn_id = user_info.mycroft_session
+        print('--> CHECKING MYCROFT CONNECTIONS:', conn_id, conn_status)
+
+        if conn_status is False:
+            return Response({"connection": "false", "access_token": conn_id})
         else:
             return Response({"connection": "true"})
 
