79f2efdf11900d81707fcc91c406e656c9233aa9
gapaza <gapaza@tamu.edu>
2022-06-16 14:19:15 -0500

    added files for QA system and slide recommender

--- EOSS/dialogue/definitions.py
+++ EOSS/dialogue/definitions.py
@@ -1,6 +1,6 @@
 
 
-### --- START MISSION PAYLOADS --- ###
+
 
 def get_definition_2020():
     return "Delta-V is the total change in spacecraft velocity over a mission lifecycle"
@@ -13,6 +13,12 @@ def get_definition_2022():
 
 def get_definition_2023():
     return "The three main approaches to cost estimation are the following: top-down, bottom-up, and anaology."
+
+
+
+
+
+### --- START MISSION PAYLOADS --- ###
 
 def get_definition_2024():
     return "There are four main remote sensor systems types: visible systems, infrared systems, radar systems, and microwave radiometers"
@@ -51,36 +57,100 @@ def get_definition_2035():
     return "At a high level, EO/IR payloads consist of a focal plane array (FPA) and readout integrated circuits (ROIC)"
 
 def get_definition_2036():
-    return "Passive thermal control systems mitigates heat passively through the natural infrared radiation of the spacecraft"
+    return "Passive thermal control systems dispel heat passively through the natural infrared radiation"
+
+### --- END MISSION PAYLOADS --- ###
+
+
+
+
+
+
+
 
 
 ### --- START SPACECRAFT BUS --- ###
 
-
 def get_definition_2037():
-    return "XXXXXXXXXXXX"
+    return "Satellite booms are extensible mechanisms made to deploy other mechanisms such as antennas or solar panels"
 
 def get_definition_2038():
-    return "XXXXXXXXXXXX"
+    return "Satellite gimbals are used to point some component in a desired direction"
 
 def get_definition_2039():
-    return "XXXXXXXXXXXX"
+    return "A low Earth orbit (LEO) is an orbit that is relatively close to Earth’s surface (<1000km but could be as low as 160km)"
 
 def get_definition_2040():
-    return "XXXXXXXXXXXX"
+    return "A geostationary orbit (GEO) is a circular geosynchronous orbit 35,786km in altitude above earth's equator. An object in such an orbit has an orbital period equal to Earth's rotational period, and so to ground observers it appears fixed in the sky"
 
 def get_definition_2041():
-    return "XXXXXXXXXXXX"
+    return "A geosynchronous orbit (GSO) is an Earth-centered orbit with an orbital period that matches Earth's rotation on its axis. An object in geosynchronous orbit returns to exactly the same position in the sky after a period of one sidereal day"
 
 def get_definition_2042():
-    return "XXXXXXXXXXXX"
+    return "The sidereal day is the time required for the Earth to rotate once relative to the background of the stars"
 
 def get_definition_2043():
-    return "XXXXXXXXXXXX"
+    return "A satellite is in eclipse when its orbiting body comes between the spacecraft and the sun"
 
 def get_definition_2044():
-    return "XXXXXXXXXXXX"
+    return "A battery cycle refers to one full drain of the battery's charge"
 
 def get_definition_2045():
-    return "XXXXXXXXXXXX"
+    return "In the context of batteries, specific energy is a measure of how much energy a battery contains in comparison to its weight (W-hr/kg)"
 
+def get_definition_2046():
+    return "EMI Shielding in electronic devices and equipment is done to protect signals from being disrupted by external electromagnetic signals."
+
+def get_definition_2047():
+    return "Spin stabilization is a attitude control method where the spacecraft spins on some axis to stabilize itself with respect to that axis"
+
+def get_definition_2048():
+    return "Three-axis stabilization is an attitude control method that allows for stabilization with respect to all three axes"
+
+def get_definition_2049():
+    return "An inertial reference unit (IRU) is a type of inertial sensor that determines a moving spacecraft's change in rotational attitude and translational position."
+
+def get_definition_2050():
+    return "Reaction wheels are used to control the attitude of a satellite without the use of thrusters. They work by equipping the spacecraft with an electric motor attached to a flywheel, which, when its rotation speed is changed, causes the spacecraft to begin to counter-rotate proportionately through conservation of angular momentum."
+
+def get_definition_2051():
+    return "A control moment gyroscope (CMG) is an attitude control device generally used in spacecraft attitude control systems. CMGs consist of a spinning rotor and one or more motorized gimbals that tilt the rotor’s angular momentum."
+
+def get_definition_2052():
+    return "A magnetorquer (also known as a torque rod) is a satellite system for attitude control. Magnetorquers create a magnetic dipole that interfaces with an ambient magnetic field, usually Earth's, so that the counter-forces produced provide useful torque. "
+
+def get_definition_2053():
+    return "A charge-coupled device (CCD) is an integrated circuit containing an array of linked, or coupled, capacitors."
+
+def get_definition_2054():
+    return "The nadir is the direction pointing directly below a particular location. For an earth orbiting spacecraft, nadir points directly at earth's center"
+
+def get_definition_2055():
+    return "Momentum dumping is when some force (such as a thruster) is applied to counteract a change in momentum caused by removing momentum from some attitude control mechanism."
+
+def get_definition_2056():
+    return "Orbital station keeping the correction of a satellites trajectory to maintain a desired one (ex. maintaining a specific orbital altitude)."
+
+def get_definition_2057():
+    return "Cross strapping is a method of adding redundancy to a system through interconnected identical units where either unit can provide the required functionality."
+
+### --- END SPACECRAFT BUS --- ###
+
+
+def get_definition_2058():
+    return "Focal plane arrays are arrays of light detectors placed in the focal plane of an imaging system."
+
+
+### --- START LIFECYCLE COST --- ###
+
+def get_definition_2059():
+    return "Economy of scale refers to a proportionate saving in costs gained by an increased level of production."
+
+def get_definition_2060():
+    return "For spacecraft, slew and slewing refers to a spacecraft's change in attitude in reference to a fixed position."
+
+def get_definition_2061():
+    return "Technology readiness levels (TRLs) are a method for estimating the maturity of technologies. The use of TRLs enables consistent, uniform discussions of technical maturity across different types of technology."
+
+def get_definition_2062():
+    return "A power law is a functional relationship between two quantities, where a relative change in one quantity results in a proportional relative change in the other quantity (one quantity varies as a power of another). Ex: the area of a square in terms of the length of a side."
