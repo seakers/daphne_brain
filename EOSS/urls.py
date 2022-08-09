676f2e3fc3a1367fd369a70558c0ae99c559ffcc
gapaza <gapaza@tamu.edu>
2022-08-09 10:11:27 -0500

    refactoring and global variable name fixing

--- EOSS/urls.py
+++ EOSS/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('teacher/', include('EOSS.teacher.urls')),
-    path('engineer/', include('EOSS.engineer.urls')),
     path('analyst/', include('EOSS.analyst.urls')),
-    path('explorer/', include('EOSS.explorer.urls')),
-    # path('historian/', include('EOSS.historian.urls')),  # No APIs for now
     path('critic/', include('EOSS.critic.urls')),
     path('data/', include('EOSS.data.urls')),
-    path('settings/', include('EOSS.settings.urls')),
     path('dialogue/', include('EOSS.dialogue.urls')),
+    path('engineer/', include('EOSS.engineer.urls')),
+    path('explorer/', include('EOSS.explorer.urls')),
+    path('settings/', include('EOSS.settings.urls')),
+    path('teacher/', include('EOSS.teacher.urls')),
     path('vassar/', include('EOSS.vassar.urls'))
 ]
