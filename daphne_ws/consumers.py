6248ced256b6f0effc44b0c7e0587b1b02d90db9
gapaza <gapaza@tamu.edu>
2022-08-10 10:53:47 -0500

    commented daphne_ws/consumers

--- daphne_ws/consumers.py
+++ daphne_ws/consumers.py
@@ -1,11 +1,89 @@
 import hashlib
-from channels.generic.websocket import AsyncJsonWebsocketConsumer, JsonWebsocketConsumer
-from asgiref.sync import async_to_sync, sync_to_async
-from channels.layers import get_channel_layer
-from daphne_ws.async_db_methods import _get_or_create_user_information, _save_subcontext, _save_user_info
 import schedule
-from auth_API.helpers import get_user_information, get_or_create_user_information
+
+
+from asgiref.sync import sync_to_async
+from channels.layers import get_channel_layer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+
+
 from daphne_context.models import UserInformation
+from daphne_ws.async_db_methods import _get_or_create_user_information, _save_subcontext, _save_user_info
+
+
+
+class DaphneConsumer(AsyncJsonWebsocketConsumer):
+    scheduler = schedule.Scheduler()
+    sched_stopper = None
+    kill_event = None
+    daphne_version = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    ##### WebSocket event handlers
+    async def connect(self):
+        """
+            Called when the websocket is handshaking as part of initial connection.
+        """
+
+        # --> 1. Accept connection
+        await self.accept()
+
+        # --> 2. Save ws channel name in user_info
+        user_information: UserInformation = await _get_or_create_user_information(self.scope['session'], self.scope['user'], self.daphne_version)
+        user_information.channel_name = self.channel_name
+        await _save_user_info(user_information)
+
+        # --> 3. Add hash-key and channel name to channel groups
+        key = self.scope['path'].lstrip('api/')
+        hash_key = hashlib.sha256(key.encode('utf-8')).hexdigest()
+        await self.channel_layer.group_add(hash_key, self.channel_name)
+
+    async def receive_json(self, content, **kwargs):
+        """
+            Called when we get a text frame. Channels will JSON-decode the payload
+            for us and pass it as the first argument.
+        """
+
+        # --> 1. Get user_info
+        user_info: UserInformation = await _get_or_create_user_information(self.scope['session'], self.scope['user'])
+
+        # --> 2. Get key and hash-key
+        key = self.scope['path'].lstrip('api/')
+        hash_key = hashlib.sha256(key.encode('utf-8')).hexdigest()
+
+        # --> 3. Handle message based on: msg_type
+        # - context_add
+        # - text_msg
+        # - ping
+        if content.get('msg_type') == 'context_add':
+            for subcontext_name, subcontext in content.get('new_context').items():
+                for key, value in subcontext.items():
+                    setattr(getattr(user_info, subcontext_name), key, value)
+                await _save_subcontext(user_info, subcontext_name)
+            await _save_user_info(user_info)
+        elif content.get('msg_type') == 'text_msg':
+            textMessage = content.get('text', None)
+            await self.channel_layer.group_send(hash_key, { "text": textMessage })
+        elif content.get('msg_type') == 'ping':
+            print("Ping received")
+            await self.send_json({
+                'type': 'ping'
+            })
+
+    async def disconnect(self, code):
+        """
+            Called when the WebSocket closes for any reason.
+        """
+
+        # --> 1. Get key and hash-key
+        key = self.scope['path'].lstrip('api/')
+        hash_key = hashlib.sha256(key.encode('utf-8')).hexdigest()
+
+        # --> 2. Remove from channel name / hash-key from channel group
+        await self.channel_layer.group_discard(hash_key, self.channel_name)
+
 
 
 
@@ -128,70 +206,3 @@ class MycroftConsumer(AsyncJsonWebsocketConsumer):
         # async_to_sync(channel_layer.send)(user_info.channel_name, message)
 
 
-class DaphneConsumer(AsyncJsonWebsocketConsumer):
-    scheduler = schedule.Scheduler()
-    sched_stopper = None
-    kill_event = None
-    daphne_version = ""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    ##### WebSocket event handlers
-    async def connect(self):
-        """
-        Called when the websocket is handshaking as part of initial connection.
-        """
-        # Accept the connection
-        await self.accept()
-
-        user_information: UserInformation = await _get_or_create_user_information(self.scope['session'], self.scope['user'], self.daphne_version)
-        print(user_information, self.scope['session'], self.scope['user'])
-        print(self.channel_name)
-
-
-        user_information.channel_name = self.channel_name
-        await _save_user_info(user_information)
-    
-        key = self.scope['path'].lstrip('api/')
-        hash_key = hashlib.sha256(key.encode('utf-8')).hexdigest()
-        # Add to the group
-        await self.channel_layer.group_add(hash_key, self.channel_name)
-
-    async def receive_json(self, content, **kwargs):
-        """
-        Called when we get a text frame. Channels will JSON-decode the payload
-        for us and pass it as the first argument.
-        """
-        key = self.scope['path'].lstrip('api/')
-        hash_key = hashlib.sha256(key.encode('utf-8')).hexdigest()
-
-        # Get an updated session store
-        user_info: UserInformation = await _get_or_create_user_information(self.scope['session'], self.scope['user'])
-
-        # Update context to SQL one
-        if content.get('msg_type') == 'context_add':
-            for subcontext_name, subcontext in content.get('new_context').items():
-                for key, value in subcontext.items():
-                    setattr(getattr(user_info, subcontext_name), key, value)
-                await _save_subcontext(user_info, subcontext_name)
-            await _save_user_info(user_info)
-        elif content.get('msg_type') == 'text_msg':
-            textMessage = content.get('text', None)
-            # Broadcast
-            await self.channel_layer.group_send(hash_key, { "text": textMessage })
-        elif content.get('msg_type') == 'ping':
-            print("Ping received")
-            await self.send_json({
-                'type': 'ping'
-            })
-
-    async def disconnect(self, code):
-        """
-        Called when the WebSocket closes for any reason.
-        """
-        # Leave all the rooms we are still in
-        key = self.scope['path'].lstrip('api/')
-        hash_key = hashlib.sha256(key.encode('utf-8')).hexdigest()
-        # Remove from the group on clean disconnect
-        await self.channel_layer.group_discard(hash_key, self.channel_name)
