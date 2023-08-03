8d5c91837a63ec23ce3b48b7ba0e8d4a021d6be1
prachichi <pd385@cornell.edu>
2023-08-03 12:52:22 -0500

    changing the neo4j IP address

--- dialogue/qa_pipeline.py
+++ dialogue/qa_pipeline.py
@@ -383,7 +383,7 @@ def query(query, data, command_class):
 def neo4j_query(query, data, command_class):
     print(query)
     print(data)
-    driver = GraphDatabase.driver("neo4j://3.15.160.239:7687", auth=basic_auth("neo4j", "goSEAKers!"))
+    driver = GraphDatabase.driver("neo4j://3.144.213.63:7687", auth=basic_auth("neo4j", "goSEAKers!"))
     session = driver.session()
 
     results = "result"
