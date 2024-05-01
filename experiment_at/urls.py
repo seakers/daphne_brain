82efc8f816dcd17023bf3b00c23660dea1233770
prachichi <pd385@cornell.edu>
2024-04-30 20:54:43 -0500

    changes related to blanking the screen during without daphne session

--- experiment_at/urls.py
+++ experiment_at/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('finish-experiment', views.FinishExperiment.as_view()),
     path('subject-list', views.SubjectList.as_view()),
     path('get-state', views.GetState.as_view()),
-    path('without-daphne-session', views.TurnOffAlarms.as_view()),
+    path('without-daphne-session', views.WithoutDaphneSession.as_view()),
     path('finish-experiment-from-mcc', views.FinishExperimentFromMcc.as_view()),
     path('force-finish-experiment-from-mcc', views.ForceFinishExperimentFromMcc.as_view()),
     path('turn-off-alarms', views.TurnOffAlarms.as_view())
